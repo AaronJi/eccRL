3af01d43abf74f9ed320503a7c57cdd996c6ab86
AaronJi <jiluo.lj@alibaba-inc.com>
2022-12-19 01:12:35 +0800

    sb3 version2

--- ecc_baselines3/common/save_util.py
+++ ecc_baselines3/common/save_util.py
@@ -16,7 +16,6 @@ from typing import Any, Dict, Optional, Tuple, Union
 import cloudpickle
 import torch as th
 
-import stable_baselines3 as sb3
 from ecc_baselines3.common.type_aliases import TensorDict
 from ecc_baselines3.common.utils import get_device, get_system_info
 
