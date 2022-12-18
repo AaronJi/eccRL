062038f97b9357bc64cd3dd877a6ac9e56eeb166
AaronJi <jiluo.lj@alibaba-inc.com>
2022-12-19 01:40:53 +0800

    sb3 version6

--- utils/hyperparams_opt.py
+++ utils/hyperparams_opt.py
@@ -2,7 +2,7 @@ from typing import Any, Dict
 
 import numpy as np
 import optuna
-from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+from ecc_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 from torch import nn as nn
 
 from utils import linear_schedule
